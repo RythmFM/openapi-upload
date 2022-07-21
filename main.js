9fc92e962f4cd5240199c4109ed3069f65045897
ByteAlex <bytealex@zerotwo.bot>
2022-07-21 13:09:36 +0200

    Import instead of require

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const SwaggerParser = require('@apidevtools/swagger-parser');
-const fetch = require("node-fetch");
+import fetch from 'node-fetch';
 
 (async () => {
     try {
